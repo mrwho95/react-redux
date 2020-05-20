2e613b8a3c60dc9fef07c245f87ed464da122439
mrwho95 <dnkh903@gmail.com>
2020-05-21 06:34:29 +0800

    install redux and redux logger in package json, create redux action creator, store and reducers

---
+++ index.js
@@ -0,0 +1,87 @@
+//if ES6
+//import redux from 'redux'
+
+//simple node js app
+const redux = require("redux");
+const createStore = redux.createStore;
+const combineReducers = redux.combineReducers;
+
+// ACTION
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+// action creator
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+function buyIceCream() {
+  return {
+    type: BUY_ICECREAM,
+    info: "Second redux action",
+  };
+}
+
+//reducer = (previous state, action) => newState
+// const initialState={
+//   numOfCakes: 10,
+//   numOfIceCream: 20,
+// }
+
+const initialCakeState = {
+  numOfCakes: 10,
+};
+
+const initialIceCreamState = {
+  numOfIceCream: 20,
+};
+
+const cakeReducer = (state = initialCakeState, action) => {
+  //return new state based on current state and the action
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state, //asking reducer to first make copy of the state object, and then update number of cakes, other properties remain unchanged.
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+  //return new state based on current state and the action
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        numOfIceCream: state.numOfIceCream - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+//redux store
+//Hold application state
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
+const store = createStore(rootReducer);
+console.log("initial state", store.getState()); //access to state via getState()
+const unsubcribe = store.subscribe(() =>
+  console.log("Updated state", store.getState())
+); //register listeners via subcribe(listener)
+store.dispatch(buyCake()); //allow state to be updated dispatch(action)
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+//handle unregistering of listeners via the function returned by subscribe(listener)
+unsubcribe();
